5de381bba8453acfbe0ecf0de1e28ac4d4d96dd9
admin <dangtinhtong@gmail.com>
2017-09-30 09:35:04 +0700

    First commit

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+ Thử sử dụng khi clone về mmáy.
+ - Lần đầu ccommint.
--- proStructure/main.m
+++ proStructure/main.m
@@ -6,12 +6,32 @@
 //  Copyright © 2017 admin. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
-    return 0;
+//
+#import <Foundation/Foundation.h>   /* is a proprocessor command, which tells a Objecticve C complier to
+                                       include Foundation.h  before going to actual compilation*/
+@interface sampleClass : NSObject    // To shows how to create  an interface. It inherits NSObject,
+                                     // which is base class of all objective
+
+-(void) sampleMethod;   // Shows how to declare a method
+@end    // Marks the end of an interface
+
+@implementation sampleClass   // Shows how to implement the interface SampleClass
+
+-(void) sampleMethod{
+    NSLog(@"Hello world.");  // is another function available in Objective-C. Which cause the message "Hello
+                            // Workl" to be displayed on the screen
+}
+
+@end  // Marks the end of an implementation
+
+
+
+int main(int argc, const char * argv[])   // is the main function where program execution begins
+{
+    sampleClass *mySampleClass =[[sampleClass alloc]init];
+    
+    [mySampleClass sampleMethod];
+    
+    return 0;// Terminates main() function and returns the value 0
 }
